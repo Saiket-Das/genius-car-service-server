b78923cb2e8de0b09f6e0c3ccaace1152b530406
Saiket Das <saiketdas02@gmail.com>
2022-04-26 17:45:34 +0800

    Adding Steps

---
+++ _server_deploys.js
@@ -0,0 +1,16 @@
+/*
+---------------------------------
+ONE TIME for each computer 
+--------------------------------- 
+1 - Create Heroku Account (One time)
+2 - Varify Email
+3 - intsall Heroku 
+4 - heroku login
+
+
+---------------------------------
+For each project one time 
+---------------------------------
+1 - heroku create 
+2 -
+*/
